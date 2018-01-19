0b5dc44ec35390fdd5dc19b6784e16acd8e2385d
Sicco van Sas <siccovansas@users.noreply.github.com>
2018-01-19 15:16:05 +0100

    Add email and file logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 #/usr/bin/env python
 # -*- coding: utf-8 -*-
 import locale
+import os
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
 from config import Config
 from flask import Flask
 from flask_babel import Babel
@@ -23,3 +26,41 @@ login_manager.login_view = "gemeente_login"
 locale.setlocale(locale.LC_NUMERIC, 'nl_NL.UTF-8')
 
 from app import routes, models, errors
+
+if not app.debug:
+    # Send email on errors
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr=app.config['FROM'],
+            toaddrs=app.config['ADMINS'],
+            subject='[Stemlokalen] website error',
+            credentials=auth,
+            secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    # Log info messages and up to file
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler(
+        'logs/stemlokalen.log',
+        maxBytes=1000000,
+        backupCount=10
+    )
+    file_handler.setFormatter(
+        logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+        )
+    )
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Stemlokalen startup')
