87bf9b965a9e797050da97a6303181b774758b9f
Breyten Ernsting <bje@dds.nl>
2018-02-08 18:33:58 +0100

    Add tests to ensure we extract the correct data.

--- tests/parser.py
+++ tests/parser.py
@@ -21,6 +21,45 @@ class TestExcelParser(unittest.TestCase):
         self.parser = ExcelParser()
         self.file_path = os.path.join(
             os.path.dirname(os.path.abspath(__file__)), 'data/juinen.xlsx')
+        self.records = [{
+            'nummer stembureau': 1.0,
+            'naam stembureau': 'Stadhuis I',
+            'gebruikersdoel het gebouw': 'Kantoor',
+            'bag referentie nummer': '0518200000747446',
+            'extra adresaanduiding': '',
+            'longitude': 4.3166395,
+            'latitude': 52.0775912,
+            'districtcode': '',
+            'openingstijden': '2017-03-21T07:30:00 tot 2017-03-21T21:00:00',
+            'mindervaliden toegankelijk': 'Y',
+            'invalidenparkeerplaatsen': 'N',
+            'akoestiek': 'N',
+            'mindervalide toilet aanwezig': 'Y',
+            'kieskring id': 'Juinen',
+            'hoofdstembureau': '',
+            'website locatie': 'https://www.juinen.nl/',
+            'contactgegevens': 'info@juinen.nl',
+            'beschikbaarheid': 'https://www.juinen.nl/verkiezingen-2018/'
+        }, {
+            'nummer stembureau': 2.0,
+            'naam stembureau': 'Stadhuis II',
+            'gebruikersdoel het gebouw': 'Kantoor',
+            'bag referentie nummer': '0518200000747446',
+            'extra adresaanduiding': '',
+            'longitude': 4.3166395,
+            'latitude': 52.0775912,
+            'districtcode': '',
+            'openingstijden': '2017-03-21T07:30:00 tot 2017-03-21T21:00:00',
+            'mindervaliden toegankelijk': 'Y',
+            'invalidenparkeerplaatsen': 'N',
+            'akoestiek': 'N',
+            'mindervalide toilet aanwezig': 'Y',
+            'kieskring id': 'Juinen',
+            'hoofdstembureau': '',
+            'website locatie': 'https://www.juinen.nl/',
+            'contactgegevens': 'info@juinen.nl',
+            'beschikbaarheid': 'https://www.juinen.nl/verkiezingen-2018/'
+        }]
 
     def test_has_correct_num_sheets(self):
         wb = open_workbook(self.file_path)
@@ -49,3 +88,10 @@ class TestExcelParser(unittest.TestCase):
             'hoofdstembureau',
             'contactgegevens',
             'beschikbaarheid'])
+
+    def test_get_rows_good(self):
+        headers, rows = self.parser.parse(self.file_path)
+        self.assertEqual(len(rows), 2)
+        self.maxDiff = None
+        self.assertDictEqual(rows[0], self.records[0])
+        self.assertDictEqual(rows[1], self.records[1])
