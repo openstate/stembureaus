d7092a8189de4466509b1000bc0f7e08afe4e9af
Sicco van Sas <siccovansas@users.noreply.github.com>
2018-03-02 13:51:12 +0100

    Update fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,15 +4,9 @@ env.use_ssh_config = True
 env.hosts = ["Oxygen"]
 
 
-def deploy_init():
-    with cd('/home/projects'):
-        run('git clone git@github.com:openstate/stembureaus.git')
-        with cd('/home/projects/stembureaus/docker'):
-            sudo('docker-compose up -d')
-
-
 def deploy():
     with cd('/home/projects/stembureaus'):
         run('git pull git@github.com:openstate/stembureaus.git')
+        sudo('docker exec -it stm_nodejs_1 gulp')
         run('touch uwsgi-touch-reload')
         #sudo('docker exec stm_nginx_1 nginx -s reload')
