3338d770cd0971bd8c870dc55c2cb1a4914dd78e
Breyten Ernsting <bje@dds.nl>
2018-03-07 15:21:40 +0100

    Whoops.

--- bin/auto_email.sh
+++ bin/auto_email.sh
@@ -3,4 +3,4 @@
 cd /opt/stm
 
 ./bin/run.py >./app/data/nieuwe-gemeenten.json
-flask eenmalig_gemeenten_en_verkiezingen_aanmaken --json_file=app/data/nieuwe_gemeenten.json >>log/auto_email.log
+ flask gemeenten eenmalig_gemeenten_en_verkiezingen_aanmaken --json_file=app/data/nieuwe-gemeenten.json >>logs/auto_email.log
