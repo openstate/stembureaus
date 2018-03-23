119a9cbf2979018559510b20ed48a88a85c45d6d
Sicco van Sas <siccovansas@users.noreply.github.com>
2018-03-23 18:59:36 +0100

    Update of convert script

--- bin/convert_groningen_data.py
+++ bin/convert_groningen_data.py
@@ -5,29 +5,13 @@ import sys
 import csv
 
 from openpyxl import load_workbook
+from openpyxl.styles import Font
 from pyexcel_ods3 import get_data, save_data
 
 sys.path.insert(0, '.')
 
 from app.utils import find_buurt_and_wijk
 from app.models import BAG
-
-
-class UnicodeReader:
-    """
-    A CSV reader which will iterate over lines in the CSV file "f",
-    which is encoded in the given encoding.
-    """
-
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        self.reader = csv.reader(f, dialect=dialect, **kwds)
-
-    def __next__(self):
-        row = next(self.reader)
-        return row
-
-    def __iter__(self):
-        return self
 
 
 def find_bag_record(data):
@@ -47,9 +31,13 @@ def find_bag_record_by_huisletter(data):
         woonplaats=data['Plaats'].strip()
     ).first()
 
+def as_text(value):
+    if value is None:
+        return ""
+    return str(value)
 
 def main():
-    reader = UnicodeReader(sys.stdin, delimiter=',')
+    reader = csv.reader(sys.stdin, delimiter=',')
     header = reader.__next__()
     points_per_muni = {}
     for row in reader:
@@ -80,39 +68,49 @@ def main():
 
     for muni, points in points_per_muni.items():
         # first Excel
-        path = "data/%s-2018-03-21.xlsx" % (muni,)
-        os.system("cp waarismijnstemlokaal.nl_invulformulier.xlsx \"%s\"" % (
+        print(muni)
+        path = "data/waarismijnstemlokaal.nl_invulformulier_%s_deels_vooringevuld.xlsx" % (muni,)
+        os.system("cp files/waarismijnstemlokaal.nl_invulformulier.xlsx \"%s\"" % (
             path,))
         workbook = load_workbook(path)
         worksheet = workbook['Attributen']
-        col = 5
+        font = Font(name="Arial", size=10)
+        col = 6
         for point in points:
-            worksheet.cell(row=2, column=col, value=point['Naam stembureau'])
+            worksheet.cell(row=3, column=col, value=point['Naam stembureau']).font = font
+
             if 'BAG referentienummer' in point:
-                worksheet.cell(row=6, column=col, value=point['BAG referentienummer'])
+                worksheet.cell(row=5, column=col, value=point['BAG referentienummer']).font = font
             if 'Longitude' in point:
-                worksheet.cell(row=8, column=col, value=point['Longitude'])
+                worksheet.cell(row=7, column=col, value=point['Longitude']).font = font
             if 'Latitude' in point:
-                worksheet.cell(row=9, column=col, value=point['Latitude'])
-            worksheet.cell(row=11, column=col, value=point['Openingstijden'])
+                worksheet.cell(row=8, column=col, value=point['Latitude']).font = font
+            worksheet.cell(row=10, column=col, value=point['Openingstijden']).font = font
             col += 1
+
+        # Set width of each column
+        for column_cells in tuple(worksheet.columns)[5:]:
+            length = max(len(as_text(cell.value)) for cell in column_cells)
+            worksheet.column_dimensions[column_cells[0].column].width = int(length * 0.75)
+
         workbook.save(path)
+        workbook.close()
 
         # now openoffice
-        path = "data/%s-2018-03-21.ods" % (muni,)
-        os.system("cp waarismijnstemlokaal.nl_invulformulier.ods \"%s\"" % (
-            path,))
-        data = get_data(path)
-        for point in points:
-            data['Attributen'][2].append(point['Naam stembureau'])
-            if 'BAG referentienummer' in point:
-                data['Attributen'][5].append(point['BAG referentienummer'])
-            if 'Longitude' in point:
-                data['Attributen'][7].append(point['Longitude'])
-            if 'Latitude' in point:
-                data['Attributen'][8].append(point['Latitude'])
-            data['Attributen'][10].append(point['Openingstijden'])
-            save_data(path, data)
+        #path = "data/%s-2018-03-21.ods" % (muni,)
+        #os.system("cp files/waarismijnstemlokaal.nl_invulformulier.ods \"%s\"" % (
+        #    path,))
+        #data = get_data(path)
+        #for point in points:
+        #    data['Attributen'][2].append(point['Naam stembureau'])
+        #    if 'BAG referentienummer' in point:
+        #        data['Attributen'][5].append(point['BAG referentienummer'])
+        #    if 'Longitude' in point:
+        #        data['Attributen'][7].append(point['Longitude'])
+        #    if 'Latitude' in point:
+        #        data['Attributen'][8].append(point['Latitude'])
+        #    data['Attributen'][10].append(point['Openingstijden'])
+        #    save_data(path, data)
     return 0
 
 if __name__ == '__main__':
