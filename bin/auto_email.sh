3b2d4986310c5b93736ece72869a613f52076bd6
Sicco van Sas <siccovansas@users.noreply.github.com>
2021-02-01 17:54:49 +0100

    Small fix

--- bin/auto_email.sh
+++ bin/auto_email.sh
@@ -3,4 +3,4 @@
 cd /opt/stm
 
 ./bin/run.py > ./app/data/nieuwe-gemeenten.json
-flask mysql add_gemeenten_verkiezingen_users --json_file=app/data/nieuwe-gemeenten.json >> logs/auto_email.log
+flask mysql add-gemeenten-verkiezingen-users --json_file=app/data/nieuwe-gemeenten.json >> logs/auto_email.log
