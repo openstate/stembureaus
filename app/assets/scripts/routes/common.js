193bf046ecf0e31fb8606db32fd26ca644bd7bd7
Breyten Ernsting <bje@dds.nl>
2022-01-06 15:51:37 +0100

    Proof of concept without any form of styling.

--- app/assets/scripts/routes/common.js
+++ app/assets/scripts/routes/common.js
@@ -1,8 +1,37 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+    console.log('javascript common loaded!');
+    // JavaScript to be fired on pages that contain the map
+    if ($('.edit-form').length) {
+      run_editform();
+    }
+
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
   },
+};
+
+var run_editform = function () {
+  console.log('attaching to edit form now!');
+  $('#bag_nummeraanduiding_id').on('keyup', function (e) {
+    var query = $(this).val();
+
+    if (query.length < 3) {
+      console.log('query not long enough');
+      return;
+    }
+
+    $.get('/t/' + encodeURIComponent(query), function (data) {
+      $('#results').empty();
+      var output = '<ul>';
+      console.dir(data);
+      $.each(data, function (idx, elem) {
+        output += '<li>' + elem.openbareruimte + ' ' + elem.huisnummer + ' ' + elem.huisnummertoevoeging + '</li>';
+      });
+      output += '</ul>';
+      $('#results').html($(output));
+    }, 'json');
+  });
 };
