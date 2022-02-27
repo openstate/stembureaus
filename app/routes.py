78dae923b9e0dfdb09ee5b3003c6fc1f58007636
Sicco van Sas <siccovansas@users.noreply.github.com>
2022-02-27 19:11:44 +0100

    Fixes/improvements to the new stembureau verwijderen on the overzicht page functionality

--- app/routes.py
+++ app/routes.py
@@ -846,10 +846,6 @@ def gemeente_stemlokalen_edit(stemlokaal_id=None):
 
 
 @app.route(
-    "/gemeente-stemlokaal-delete",
-    methods=['GET', 'POST']
-)
-@app.route(
     "/gemeente-stemlokaal-delete/<stemlokaal_id>",
     methods=['GET', 'POST']
 )
@@ -864,25 +860,6 @@ def gemeente_stemlokaal_delete(stemlokaal_id=None):
     ).first()
     elections = gemeente.elections.all()
 
-    # need this to get a starting point for the clickmap
-    bag_record = BAG.query.filter_by(
-        gemeente=gemeente.gemeente_naam
-    ).order_by('openbareruimte').first()
-
-    # Pick the first election. In the case of multiple elections we only
-    # retrieve the stembureaus of the first election as the records for
-    # both elections are the same (at least the GR2018 + referendum
-    # elections on March 21st 2018).
-    verkiezing = elections[0].verkiezing
-
-    all_draft_records = ckan.get_records(
-        ckan.elections[verkiezing]['draft_resource']
-    )
-
-    gemeente_draft_records = [
-        record for record in all_draft_records['records']
-        if record['CBS gemeentecode'] == gemeente.gemeente_code
-    ]
     if stemlokaal_id:
         for election in [x.verkiezing for x in elections]:
             ckan.delete_records(
