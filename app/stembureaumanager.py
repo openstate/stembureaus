1637d3529815edb56a3d34e57345061b14107b07
Breyten Ernsting <bje@dds.nl>
2023-02-07 16:35:16 +0100

    Working proof of concept of validating stuff via the api

--- app/stembureaumanager.py
+++ app/stembureaumanager.py
@@ -1,7 +1,7 @@
 from app import app, db
 from app.models import Gemeente, User, Gemeente_user, Election, BAG, ckan, add_user, Record
 from app.email import send_invite, send_update
-from app.parser import UploadFileParser
+from app.parser import BaseParser, UploadFileParser, valid_headers
 from app.validator import Validator
 from app.routes import _remove_id, _create_record, kieskringen
 from app.utils import find_buurt_and_wijk
@@ -13,6 +13,50 @@ from pprint import pprint
 from dateutil import parser
 import requests
 from flask import url_for
+
+class StembureauManagerParser(BaseParser):
+    def convert_to_record(self, data):
+        return {
+            'nummer_stembureau': data['Nummer stembureau'],
+            'naam_stembureau': data['Naam stembureau'],
+            'type_stembureau': data['Type stembureau'],
+            'website_locatie': data['Locaties'][0]['Website locatie'],
+            'bag_nummeraanduiding_id': data['Locaties'][0]['BAG Nummeraanduiding ID'],
+            'extra_adresaanduiding': data['Locaties'][0]['Extra adresaanduiding'],
+            'latitude': str(data['Locaties'][0]['Latitude']),
+            'longitude': str(data['Locaties'][0]['Longitude']),
+            # 'x': None,
+            # 'y': None,
+            'openingstijd': data['Locaties'][0]['Openingstijden'][0]['Openingstijd'],
+            'sluitingstijd': data['Locaties'][0]['Openingstijden'][0]['Sluitingstijd'],
+            'toegankelijk_voor_mensen_met_een_lichamelijke_beperking': data['Locaties'][0][
+                'Toegankelijk voor mensen met een lichamelijke beperking'],
+            'toegankelijke_ov_halte': data['Locaties'][0]['Toegankelijke ov-halte'],
+            'akoestiek': data['Locaties'][0]['Akoestiek'],
+            'auditieve_hulpmiddelen': data['Locaties'][0]['Auditieve hulpmiddelen'],
+            'visuele_hulpmiddelen': data['Locaties'][0]['Visuele hulpmiddelen'],
+            'gehandicaptentoilet': data['Locaties'][0]['Gehandicaptentoilet'],
+            'extra_toegankelijkheidsinformatie': data['Locaties'][0]['Extra toegankelijkheidsinformatie'],
+            'tellocatie': data['Locaties'][0]['Tellocatie'],
+            'contactgegevens_gemeente': data['Contactgegevens gemeente'],
+            'verkiezingswebsite_gemeente': data['Verkiezingswebsite gemeente'],
+            'verkiezingen': data['Verkiezingen']
+        }
+
+    def _get_records(self, data, headers):
+        result = []
+        pprint(data)
+        for d in data:
+            r = self.convert_to_record(d)
+            result.append(r)
+        return result
+
+    def parse(self, path):
+        headers = valid_headers
+        records = self._get_records(path, headers)
+        clean_records = self._clean_records(records)
+        return clean_records
+
 
 class StembureauManager(object):
     def __init__(self, *arg, **kwargs):
@@ -33,35 +77,6 @@ class StembureauManager(object):
         #  https://test-opendata.stembureaumanager.nl/api/stembureau/gemeente?id=GM<code>
         return self._request('gemeente', params={'id': municipality_id})
 
-    def convert_to_record(self, data):
-        pprint(data)
-        return Record(**{
-            'nummer stembureau': data['Nummer stembureau'],
-            'naam stembureau': data['Naam stembureau'],
-            'type stembureau': data['Type stembureau'],
-            'website locatie': data['Locaties'][0]['Website locatie'],
-            'bag nummeraanduiding id': data['Locaties'][0]['BAG Nummeraanduiding ID'],
-            'extra adresaanduiding': data['Locaties'][0]['Extra adresaanduiding'],
-            'latitude': data['Locaties'][0]['Latitude'],
-            'longitude': data['Locaties'][0]['Longitude'],
-            'x': None,
-            'y': None,
-            'openingstijd': data['Locaties'][0]['Openingstijden'][0]['Openingstijd'],
-            'sluitingstijd': data['Locaties'][0]['Openingstijden'][0]['Sluitingstijd'],
-            'toegankelijk voor mensen met een lichamelijke beperking': data['Locaties'][0][
-                'Toegankelijk voor mensen met een lichamelijke beperking'],
-            'toegankelijke ov-halte': data['Locaties'][0]['Toegankelijke ov-halte'],
-            'akoestiek': data['Locaties'][0]['Akoestiek'],
-            'auditieve hulpmiddelen': data['Locaties'][0]['Auditieve hulpmiddelen'],
-            'visuele hulpmiddelen': data['Locaties'][0]['Visuele hulpmiddelen'],
-            'gehandicaptentoilet': data['Locaties'][0]['Gehandicaptentoilet'],
-            'extra toegankelijkheidsinformatie': data['Locaties'][0]['Extra toegankelijkheidsinformatie'],
-            'tellocatie': data['Locaties'][0]['Tellocatie'],
-            'contactgegevens gemeente': data['Contactgegevens gemeente'],
-            'verkiezingswebsite gemeente': data['Verkiezingswebsite gemeente'],
-            'verkiezingen': data['Verkiezingen']
-        })
-
     def run(self):
         municipalities = self.overview()
         for m in municipalities:
@@ -73,7 +88,8 @@ class StembureauManager(object):
             #if data.get('statusCode', 200) >= 400:
                 print("Could not get data for %s" % (m,))
                 continue
-            pprint(data)
-            for d in data:
-                r = self.convert_to_record(d)
-        #pprint(result)
+            records = StembureauManagerParser().parse(data)
+            pprint(records[0])
+            validator = Validator()
+            results = validator.validate(records)
+            pprint(results)
