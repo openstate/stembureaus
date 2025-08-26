8414e0089ac836e175bb12869eb1ab030bb38323
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2025-08-26 14:36:42 +0200

    Enables again sending errors from API importing

--- app/procura.py
+++ app/procura.py
@@ -160,8 +160,8 @@ class ProcuraManager(APIManager):
 
             if not results['no_errors']:
                 print("Errors were found in the results")
-#                uncomment next line about 4 weeks before the elections
-#                self._send_error_email(gemeente, records, results, SOURCE_STRING)
+#               uncomment next line about 4 weeks before the elections
+                self._send_error_email(gemeente, records, results, SOURCE_STRING)
                 continue
 
             self._save_draft_records(gemeente, gemeente_draft_records, elections, results)
