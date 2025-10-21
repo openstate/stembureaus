7f63e997214de12aafff41191b985a333f23e515
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2025-10-21 15:17:11 +0200

    Adds specific latitude/longitude for some TSA stembureaus

--- app/tsa.py
+++ app/tsa.py
@@ -166,6 +166,7 @@ class TSAManager(APIManager):
                 continue
 
             records = TSAParser().parse(data)
+            records = self._bugfix(gemeente, records)
             validator = Validator()
             results = validator.validate(records)
 
@@ -179,3 +180,25 @@ class TSAManager(APIManager):
 
             gemeente.source = SOURCE_STRING
             db.session.commit()
+
+    # To fix some (hopefully temporary) errors for some stembureaus
+    def _bugfix(self, gemeente, records):
+        if gemeente.gemeente_code == 'GM0150': # Deventer
+            for index, record in enumerate(records):
+                if record['nummer_stembureau'] == 113:
+                    self._fix_latlon(record, records, index, "52.26087196869886", "6.153974536599161")
+
+        if gemeente.gemeente_code == 'GM0779': # Geertruidenberg
+            for index, record in enumerate(records):
+                if record['nummer_stembureau'] == 6:
+                    self._fix_latlon(record, records, index, "51.68887590155298", "4.872133492118168")
+                if record['nummer_stembureau'] == 4:
+                    self._fix_latlon(record, records, index, "51.703441296183556", "4.871675635316138")
+
+        return records
+
+    def _fix_latlon(self, record, records, index, latitude, longitude):
+        if not record['latitude'] or record['latitude'] == 'None':
+            records[index]['latitude'] = latitude
+        if not record['longitude'] or record['longitude'] == 'None':
+            records[index]['longitude'] = longitude
