914c7784f98d343b67f38938fbf6bc01981e3ef7
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2025-11-25 16:52:59 +0100

    Removes obsolete run.py and cleans up requirements.txt

--- bin/auto_email.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd /opt/stm
-
-./bin/run.py > ./app/data/nieuwe-gemeenten.json
-flask mysql add-gemeenten-verkiezingen-users --json_file=app/data/nieuwe-gemeenten.json >> logs/auto_email.log
