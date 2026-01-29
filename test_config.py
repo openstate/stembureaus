41984d6dbe47e13167fa9190122810ac851b1f19
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2026-01-29 09:56:43 +0100

    Adds separate mysql docker container for testing

---
+++ test_config.py
@@ -0,0 +1,7 @@
+
+from config import Config
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:wims_tests@mysql-tests:3306/stembureaus_tests?local_infile=True'
+    WTF_CSRF_ENABLED = False
